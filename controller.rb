e3c0dcefad79f10a43eccaf343715efc06d6360c
Christian Geib <christian.geib79@gmail.com>
2018-08-07 08:27:36 +0100

    second commit

--- controller.rb
+++ controller.rb
@@ -5,8 +5,17 @@ require('pry-byebug')
 require_relative('./models/rps')
 also_reload('./models*')
 
+
 get '/response/:first_hand/:second_hand' do
   rps = Rps.new(params[:first_hand].to_i(), params[:second_hand].to_i)
   @rps = rps.response()
   erb(:result)
+end
+
+get '/' do
+  erb(:home)
+end
+
+get '/welcome' do
+  erb(:welcome)
 end
